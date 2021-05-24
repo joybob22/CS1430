682bf5233b2ca649207a1a8c3aa20c8cca88ecb1
joybob22 <braydenlemke8@gmail.com>
2021-05-24 17:13:25 -0600

    js bday project

---
+++ js-birthday/app.js
@@ -0,0 +1,8 @@
+let name = prompt("What is your name?");
+let age = prompt("How old are you?");
+
+if (age < 50) {
+    alert(`Hello ${name}, you are ${age} years old! Staying young!`);
+} else {
+    alert(`Hello ${name}, you are ${age} years old! You ain't young anymore!`);
+}
